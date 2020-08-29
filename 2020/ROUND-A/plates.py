016add8a82a7d997e93c599683ba2bd2198e593d
arthurvergacas <arthurvdm@gmail.com>
2020-08-29 02:51:18 -0300

    implemented dynamic programmng in factorial func

--- 2020/ROUND-A/plates.py
+++ 2020/ROUND-A/plates.py
@@ -3,23 +3,43 @@
 
 # This is a sub-optimal solution
 
+# recursive factorial
+
 def factorial(num):
     if num == 1:
         return 1
     return num * factorial(num - 1)
 
 
-tests = int(input())
-for x in range(tests):
-    numOfStack, quantity, desired = [int(i) for i in input().split()]
-    platesLeft = desired
-    beautyScore = 0
-    stacks = []
-    for i in range(numOfStack):
-        stacks.append([int(i) for i in input().split()])
+# factorial using dynamic programming
+memo = {}
 
-    # loop for each possible solution
-    allPossible = factorial(quantity * numOfStack) / \
-        factorial(desired) * factorial(quantity * numOfStack - desired)
-    for _ in range(allPossible):
-        pass
+
+def dynamicFactorial(num):
+    global memo
+
+    if num in memo:
+        return memo[num]
+
+    if num == 1:
+        return 1
+
+    result = num * factorial(num - 1)
+    memo[num] = result
+    return result
+
+
+# tests = int(input())
+# for x in range(tests):
+#     numOfStack, quantity, desired = [int(i) for i in input().split()]
+#     platesLeft = desired
+#     beautyScore = 0
+#     stacks = []
+#     for i in range(numOfStack):
+#         stacks.append([int(i) for i in input().split()])
+
+#     # loop for each possible solution
+#     allPossible = factorial(quantity * numOfStack) / \
+#         factorial(desired) * factorial(quantity * numOfStack - desired)
+#     for _ in range(allPossible):
+#         pass
